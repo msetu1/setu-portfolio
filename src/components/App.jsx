c9021251cc5dcfbe0ae67c2df6822bec799a19f7
msetu1 <msetu5763@gmail.com>
2024-12-11 16:34:40 +0600

    portfolio update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,53 +4,77 @@ import { FaLinkedin } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import ParticlesContainer from "../particles/ParticlesContainer";
-import { BiLogoNetlify } from "react-icons/bi";
+import { Typewriter } from "react-simple-typewriter";
+
 const App = () => {
-    return (
-        <div className=" min-h-screen bg-[#060417] text-primary-content">
-          <ParticlesContainer></ParticlesContainer>
-            <div className=" lg:max-w-4xl lg:mx-auto lg:pt-48 pt-10 mx-10 ">
-            <div className="">
-            <h2 className="text-2xl font-semibold mb-3">Assalamu-Alaikum</h2>
-          <h1 className="text-xl font-semibold mb-2">
-            My Name is  {""}
-            <span className="text-5xl text-[#15803d] font-bold">
-              Setu Akther
-            </span>
-          </h1>
-          <h3 className="text-xl font-semibold">I am a Front-end Developer</h3>
-          <p className="  my-4">
-          I am a Front-end web developer with expertise in creating attractive, user-friendly websites. Specializing in clean, responsive designs using HTML, CSS, Tailwind, JavaScript, and React. Proficient in Node.js, and MongoDB. Eager to enhance your team – let's connect! Email me or use the provided information.
+  return (
+    <div className=" min-h-screen bg-[#060417] text-primary-content">
+      <ParticlesContainer></ParticlesContainer>
+      <div className=" lg:max-w-4xl lg:mx-auto lg:pt-48 pt-10 mx-10 ">
+        <div className="">
+          <h2 className="text-2xl font-semibold mb-10">Hello !!</h2>
+         
+          <div className="flex gap-2">
+            <p className="text-[30px] font-bold">I am </p>
+            <div
+            style={{ fontSize: "30px", textAlign: "center",color:'#15803d', fontWeight: "bold" }}
+          >
+            <Typewriter
+              words={[
+                "MERN Stack Developer",
+                "Front end Developer",
+                "React Developer",
+              ]}
+              loop={5} 
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </div>
+          </div>
+          <p className=" my-4 max-w-[600px]">
+            I am a MERN Stack web developer with expertise in creating
+            attractive, user-friendly websites. Specializing in clean,
+            responsive designs using HTML, CSS, Tailwind, JavaScript, and React.
+            Proficient in Node.js, and MongoDB. Eager to enhance your team –
+            let's connect! Email me or use the provided information.
           </p>
           <div className="flex gap-5">
-          <a href="/public/setu-resume/Setu Resume.pdf" download>
-          <button className="btn text-left btn-outline border-0 border-[#15803d] hover:bg-black hover:border-[#15803d] hover:border-2 border-b-4 text-xl text-primary-content ">
-          <MdOutlineFileDownload /> My resume
-          </button>
-          </a>
-          <button className="btn text-left btn-outline border-0 border-[#15803d] hover:bg-black hover:border-[#15803d] hover:border-2 border-b-4 text-xl text-primary-content">
-          <NavLink to='/dashboard'>Explore Now</NavLink>
-          </button>
+            <a href="/public/setu-resume/Setu Resume.pdf" download>
+              <button className="btn text-left btn-outline border-0 border-[#15803d] hover:bg-black hover:border-[#15803d] hover:border-2 border-b-4 text-xl text-primary-content ">
+                <MdOutlineFileDownload /> My resume
+              </button>
+            </a>
+            <button className="btn text-left btn-outline border-0 border-[#15803d] hover:bg-black hover:border-[#15803d] hover:border-2 border-b-4 text-xl text-primary-content">
+              <NavLink to="/dashboard">Explore Now</NavLink>
+            </button>
           </div>
           <div className="flex mt-6 gap-4">
-          <div className="bg-slate-700 rounded-full p-3 shadow-lg hover:bg-green-600 ">
-            <a href="https://app.netlify.com/teams/msetu1/sites"> <BiLogoNetlify className=" hover:text-white text-[#0d9488] text-2xl  " /></a>
-            </div>
             <div className="bg-slate-700 rounded-full p-3 shadow-lg hover:bg-green-600 ">
-            <a href="https://www.facebook.com/profile.php?id=100093013625415"> <FaFacebookF className=" hover:text-white text-primary-content text-2xl  " /></a>
+              <a href="https://www.facebook.com/profile.php?id=100093013625415">
+                {" "}
+                <FaFacebookF className=" hover:text-white text-primary-content text-2xl  " />
+              </a>
             </div>
             <div className="bg-slate-700 rounded-full p-3 shadow-lg hover:bg-green-600">
-            <a href="https://github.com/msetu1"> <FaGithub className="hover:text-white text-primary-content text-2xl  " /></a>
+              <a href="https://github.com/msetu1">
+                {" "}
+                <FaGithub className="hover:text-white text-primary-content text-2xl  " />
+              </a>
             </div>
             <div className="bg-slate-700 rounded-full p-3 shadow-lg hover:bg-green-600">
-            <a href="https://www.linkedin.com/feed/"> <FaLinkedin className="hover:text-white text-primary-content text-2xl  " /></a>
+              <a href="https://www.linkedin.com/feed/">
+                {" "}
+                <FaLinkedin className="hover:text-white text-primary-content text-2xl  " />
+              </a>
             </div>
           </div>
-            </div>
-            </div>
-            
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/dashboard/example.jsx
@@ -0,0 +1,113 @@
+import { MdArrowOutward } from "react-icons/md";
+import { FaGithub } from "react-icons/fa6";
+import { motion } from "framer-motion"
+import "../css/hover-card.css"
+
+const Projects = () => {
+  return (
+    <div className="py-12 px-10 bg-gradient-to-l from-[#2234AE] from-0%  to-[#000000] to-40%  text-primary-content">
+      <div className="mb-12">
+        <h1 className="text-4xl  font-bold text-[#15803d] mb-4 underline underline-offset-8">Projects</h1>
+        <p className="">I like exploring and learning new. I always build projects try out new tools and concepts.</p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6  ">
+        <div>
+        <div className="border border-gray-700 rounded-lg relative">
+          <a href="https://brainwave-learning.netlify.app/">
+          <div className="hover-card">
+          <img className="w-full h-[350px]" src="https://i.ibb.co.com/SNKRbNL/Screenshot-2024-12-06-000901.png" alt="" />
+          </div>
+          </a>
+        </div>
+        </div>
+        <div>
+        <div className="border border-gray-700 rounded-lg relative">
+         <a href="https://assignment-12-client-sid-1307f.web.app/">
+          <div className="hover-card">
+          <img className="w-full h-[350px]" src="https://i.ibb.co.com/HP8MMsv/Screenshot-2024-12-06-002457.png" alt="" />
+          </div>
+         </a>
+        </div>
+        </div>
+        <div>
+        <div className="border border-gray-700 rounded-lg relative">
+          <img className="w-full h-[350px]" src="https://i.ibb.co.com/qj0ZzYc/Screenshot-2024-12-06-002636.png" alt="" />
+          <div className="absolute top-64 left-5">
+          <div className="mt-4 flex items-center gap-3">
+            <a href="https://github.com/msetu1/kester-soccer-club">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-black flex items-center gap-3 bg-green-800 text-white ">Github <FaGithub/></button>
+            </a>
+            <a href="https://brand-shop-client-side-d9c6a.web.app/">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-green-800 flex items-center gap-3 hover:text-white">Live Link <MdArrowOutward /></button>
+            </a>
+          </div>
+          </div>
+        </div>
+        </div>
+        <motion.div
+      whileHover={{
+        scale: 0.9,
+        transition: { duration: 0.5 },
+      }}
+      whileTap={{ scale: 0.9 }}>
+        <div className="border border-gray-700 rounded-lg relative">
+          <img className="w-full h-[350px]" src="https://i.ibb.co.com/nspYRvw/Screenshot-2024-12-06-005657.png" alt="" />
+          <div className="absolute top-64 left-5">
+          <div className="mt-4 flex items-center gap-3">
+            <a href="https://github.com/msetu1/kester-soccer-club">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-black flex items-center gap-3 bg-green-800 text-white ">Github <FaGithub/></button>
+            </a>
+            <a href="https://kester-soccer-club-project.netlify.app/">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-green-800 flex items-center gap-3 hover:text-white">Live Link <MdArrowOutward /></button>
+            </a>
+          </div>
+          </div>
+        </div>
+        </motion.div>
+        <motion.div
+      whileHover={{
+        scale: 0.9,
+        transition: { duration: 0.5 },
+      }}
+      whileTap={{ scale: 0.9 }}>
+        <div className="border border-gray-700 rounded-lg relative">
+          <img className="w-full h-[350px]" src="https://i.ibb.co.com/r3y2M1H/Screenshot-2024-12-06-005332.png" alt="" />
+          <div className="absolute top-64 left-5">
+          <div className="mt-4 flex items-center gap-3">
+            <a href="https://github.com/msetu1/kester-soccer-club">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-black flex items-center gap-3 bg-green-800 text-white ">Github <FaGithub/></button>
+            </a>
+            <a href="https://kester-soccer-club-project.netlify.app/">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-green-800 flex items-center gap-3 hover:text-white">Live Link <MdArrowOutward /></button>
+            </a>
+          </div>
+          </div>
+        </div>
+        </motion.div>
+        <motion.div
+      whileHover={{
+        scale: 0.9,
+        transition: { duration: 0.5 },
+      }}
+      whileTap={{ scale: 0.9 }}>
+        <div className="border border-gray-700 rounded-lg relative">
+          <img className="w-full h-[350px]" src="https://i.ibb.co/3pnx069/Screenshot-2024-06-22-193333.png" alt="" />
+          <div className="absolute top-64 left-5">
+          <div className="mt-4 flex items-center gap-3">
+            <a href="https://github.com/msetu1/kester-soccer-club">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-black flex items-center gap-3 bg-green-800 text-white ">Github <FaGithub/></button>
+            </a>
+            <a href="https://kester-soccer-club-project.netlify.app/">
+            <button className="border-2 border-[#15803d] px-6 py-2 lg:text-xl md:text-xl font-bold rounded-lg hover:bg-green-800 flex items-center gap-3 hover:text-white">Live Link <MdArrowOutward /></button>
+            </a>
+          </div>
+          </div>
+        </div>
+        </motion.div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
