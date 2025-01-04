5c8e6b3b52660f35e1e5179ce24ae4bbe461f7cf
msetu1 <msetu5763@gmail.com>
2025-01-05 01:15:54 +0600

    them add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,65 @@ import { MdOutlineFileDownload } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import ParticlesContainer from "../particles/ParticlesContainer";
 import { Typewriter } from "react-simple-typewriter";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  // theme
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("black", "dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
-    <div className=" min-h-screen bg-[#060417] text-primary-content">
+    <div className="min-h-screen bg-[#060417] text-primary-content">
       <ParticlesContainer></ParticlesContainer>
+      <div className="flex justify-end pr-20 pt-10">
+        <label className="grid cursor-pointer place-items-center">
+          <input
+            type="checkbox"
+            onChange={handleToggle}
+            className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1"
+          />
+          <svg
+            className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <svg
+            className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
+      </div>
       <div className=" lg:max-w-4xl lg:mx-auto lg:pt-48 pt-10 mx-10 ">
         <div className="">
           <h2 className="text-2xl font-semibold mb-10">Hello !!</h2>
